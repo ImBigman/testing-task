1f409dda0bbb92c4c5ff40a3326de804c13c3f69
ImBigman <b1gmanever@gmail.com>
2020-06-18 15:26:03 +0300

    fix

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,11 +1,9 @@
 module Api
   module V1
     class ProjectsController < BaseController
-
       def index
-        render json: { message: “ok” }
+        render json: { message: 'ok' }
       end
-
     end
   end
 end
